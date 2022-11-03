b817787580452ab377773d2fb5b7aef8fa3bd7a6
Filipe Aguiar <filipeaguiar@users.noreply.github.com>
2022-11-03 18:35:18 -0300

    Create getgame.sh

---
+++ getgame.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+SYSTEM=$1
+GAME=$2
+
+function archive () {
+  /roms/ports/.ia/ia download --no-directories $1 --glob="${2}" --destdir /roms/$SYSTEM
+}
+
+case $SYSTEM in
+  psx)
+    archive rr-sony-playstation-u "usa/${GAME}"
+    cd /roms/psx/usa
+    7zr e "*.7z"
+    mv *.bin ../
+    mv *.cue ../
+    rm -rf /roms/psx/usa
+    ;;
+  gba)
+    archive nointro.gba "${GAME}"
+    cd /roms/gba
+    7zr e "*.7z"
+    rm *.7z
+    ;;
+  gbc)
+    archive nointro.gbc "${GAME}*USA*"
+    archive nointro.gb "${GAME}*USA*"
+    cd /roms/gbc
+    7zr e "*.7z" 
+    rm *.7z
+    ;;
+  snes)
+    archive nointro.snes "${GAME}*USA*"
+    cd /roms/snes
+    7zr e "*.7z" 
+    rm *.7z
+    ;;
+  mastersystem)
+    archive nointro.ms-mkiii "${GAME}"
+    cd /roms/mastersystem
+    7zr e "*.7z" 
+    rm *.7z
+    ;;
+  megadrive)
+    archive nointro.md "${GAME}*USA*"
+    cd /roms/megadrive
+    7zr e "*.7z" 
+    rm *.7z
+    ;;
+  fbneo)
+    archive fbnarcade-fullnonmerged "arcade/${GAME}"
+    cd /roms/fbneo/arcade
+    mv *.zip ../
+    rm -rf /roms/fbneo/arcade
+    ;;
+  nds)
+    archive no-ndsdec2021 "${GAME}"
+    ;;
+  n64)
+    archive nointro.n64 "${GAME}"
+    cd /roms/n64
+    7zr e "*.7z" 
+    rm *.7z
+    ;;
+  dreamcast)
+    archive redump.dc.revival "${GAME}"
+    cd /roms/dreamcast
+    unzip "*.zip"
+    rm *.zip
+    ;;
+  psp)
+    archive redump.psp "${GAME}"
+    archive redump.psp.p2 "${GAME}"
+    cd /roms/psp
+    unzip "*.zip"
+    rm -rf *.zip
+    ;;
+  scummvm)
+    echo "TBD"
+    ;;
+  *)
+    echo "Opção Inválida"
+    ;;
+esac
